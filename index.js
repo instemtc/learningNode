3906955293a3a03ca7f50f803cef04625f2ecea1
Yogeshwar Dilta <diltayogi@gmail.com>
2019-12-25 20:12:18 +0530

    Request path experiments

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 //Dependencies
 var http = require('http');
+var url = require('url');
 
 // create a server 
 var server = http.createServer(function(req, res){
+    console.log(req);
+
+    //Get the URL
+    var parsedUrl = url.parse(req.url, true);
+
+    //Get the path
+    var path = parsedUrl.pathname;
+
     res.end("Hello World!\n");
+
+    console.log("\n\nThe req.url is:  "+ req.url);
+    console.log("\n\n The parsedUrl is: " + parsedUrl);
+    console.log("\n\n Pathame is:   " + path);
 });
 
 // start the server and listen on port 3000
