dc6e1789049c67056f319b59ef5203d446a89b26
Yogeshwar Dilta <diltayogi@gmail.com>
2019-12-25 19:39:30 +0530

    starting the server

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+//Dependencies
+var http = require('http');
+
+// create a server 
+var server = http.createServer(function(req, res){
+    res.end("Hello World!\n");
+});
+
+// start the server and listen on port 3000
+server.listen(3000, function(){
+    console.log("The server is listening on port 3000\n");
+});
+
+//Type http://127.0.0.1:3000/ in your browser   OR http://[::1]:3000/
